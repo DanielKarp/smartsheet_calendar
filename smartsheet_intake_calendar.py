673589aff244ced3ab08a237e870efb116b396b1
Daniel Karpelevitch <daniel@karpelevitch.net>
2021-08-16 12:19:05 -0700

    black formatting

--- smartsheet_intake_calendar.py
+++ smartsheet_intake_calendar.py
@@ -9,25 +9,64 @@ import smartsheet
 INTAKE_FORM_SHEET = 3901696217769860
 CALENDAR_SHEET = 4620060233885572
 
-COLORS = ["none", "#000000", "#FFFFFF", "transparent",
-          "#FFEBEE", "#FFF3DF", "#FFFEE6", "#E7F5E9", "#E2F2FE", "#F4E4F5", "#F2E8DE", "#FFCCD2", "#FFE1AF", "#FEFF85",
-          "#C6E7C8", "#B9DDFC", "#EBC7EF", "#EEDCCA", "#E5E5E5", "#F87E7D", "#FFCD7A", "#FEFF00", "#7ED085", "#5FB3F9",
-          "#D190DA", "#D0AF8F", "#BDBDBD", "#EA352E", "#FF8D00", "#FFED00", "#40B14B", "#1061C3", "#9210AD", "#974C00",
-          "#757575", "#991310", "#EA5000", "#EBC700", "#237F2E", "#0B347D", "#61058B", "#592C00"]
+COLORS = [
+    "none",
+    "#000000",
+    "#FFFFFF",
+    "transparent",
+    "#FFEBEE",
+    "#FFF3DF",
+    "#FFFEE6",
+    "#E7F5E9",
+    "#E2F2FE",
+    "#F4E4F5",
+    "#F2E8DE",
+    "#FFCCD2",
+    "#FFE1AF",
+    "#FEFF85",
+    "#C6E7C8",
+    "#B9DDFC",
+    "#EBC7EF",
+    "#EEDCCA",
+    "#E5E5E5",
+    "#F87E7D",
+    "#FFCD7A",
+    "#FEFF00",
+    "#7ED085",
+    "#5FB3F9",
+    "#D190DA",
+    "#D0AF8F",
+    "#BDBDBD",
+    "#EA352E",
+    "#FF8D00",
+    "#FFED00",
+    "#40B14B",
+    "#1061C3",
+    "#9210AD",
+    "#974C00",
+    "#757575",
+    "#991310",
+    "#EA5000",
+    "#EBC700",
+    "#237F2E",
+    "#0B347D",
+    "#61058B",
+    "#592C00",
+]
 COLOR_INDEX = list([i for i, _ in enumerate(COLORS)][4:])
 color_cycle = cycle(COLOR_INDEX)
 
-fmt_str = '%(levelname)s:%(asctime)s:%(name)s: %(message)s'
+fmt_str = "%(levelname)s:%(asctime)s:%(name)s: %(message)s"
 formatter = logging.Formatter(fmt_str)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-d_file_handler = logging.FileHandler('api_and_calendar.log')
+d_file_handler = logging.FileHandler("api_and_calendar.log")
 d_file_handler.setLevel(logging.DEBUG)
 d_file_handler.setFormatter(formatter)
 
-i_file_handler = logging.FileHandler('calendar.log')
+i_file_handler = logging.FileHandler("calendar.log")
 i_file_handler.setLevel(logging.INFO)
 i_file_handler.setFormatter(formatter)
 
@@ -39,17 +78,17 @@ logger.addHandler(i_file_handler)
 logger.addHandler(d_file_handler)
 logger.addHandler(stream_handler)
 
-s_file_handler = logging.FileHandler('api_and_calendar.log')
+s_file_handler = logging.FileHandler("api_and_calendar.log")
 s_file_handler.setLevel(logging.INFO)
 s_file_handler.setFormatter(formatter)
 
-s_logger = logging.getLogger('smartsheet.smartsheet')
+s_logger = logging.getLogger("smartsheet.smartsheet")
 s_logger.addHandler(d_file_handler)
 s_logger.setLevel(logging.DEBUG)
 
 smart = smartsheet.Smartsheet()  # use 'SMARTSHEET_ACCESS_TOKEN' env variable
 smart.errors_as_exceptions(True)
-CHANGE_AGENT = 'dkarpele_smartsheet_calendar'
+CHANGE_AGENT = "dkarpele_smartsheet_calendar"
 smart.with_change_agent(CHANGE_AGENT)
 
 
@@ -59,22 +98,24 @@ def process_sheet(sheet_id):
     rows = sheet.rows
     columns = sheet.columns
     col_map = column_name_to_id_map(columns=columns)
-    date_cols = [col for col in columns if col.type == 'DATE']
-    logger.debug(f'found {len(columns)} total columns')
-    logger.debug(f'found {len(date_cols)} date-type columns')
-    logger.debug(f'found {len(rows)} rows')
+    date_cols = [col for col in columns if col.type == "DATE"]
+    logger.debug(f"found {len(columns)} total columns")
+    logger.debug(f"found {len(date_cols)} date-type columns")
+    logger.debug(f"found {len(rows)} rows")
     for row in rows:
         event = get_cell_by_column_name(row, "Event Name", col_map).value
-        if match(r'^Q[1-4] FY\d{2}', event):  # if the row matches, it is a label row. Contains no data so it's skipped
-            logger.debug(f'{event} was identified as a separator row')
+        if match(
+            r"^Q[1-4] FY\d{2}", event
+        ):  # if the row matches, it is a label row. Contains no data so it's skipped
+            logger.debug(f"{event} was identified as a separator row")
             continue
-        logger.debug(f'{event} is being processed')
+        logger.debug(f"{event} is being processed")
         color = next(color_cycle)  # each event gets its own color
         event = replace_event_names(event)  # do some filtering to shorten some words
 
         for date_col in date_cols:
             item = replace_event_names(date_col.title)
-            name = f'{event} | {item}'
+            name = f"{event} | {item}"
             date = row.get_column(date_col.id).value
             new_cells.append((name, date, color))
 
@@ -84,15 +125,18 @@ def process_sheet(sheet_id):
 
 
 def replace_event_names(event: str) -> str:
-    replacements = [('Cisco Live', 'CL'),
-                    ('Cisco ', ''),
-                    ('Partner Summit', 'PS'),
-                    ('Date', ''),
-                    ]
+    replacements = [
+        ("Cisco Live", "CL"),
+        ("Cisco ", ""),
+        ("Partner Summit", "PS"),
+        ("Date", ""),
+    ]
     for original, new in replacements:
         if original in event:
             new_event = event.replace(original, new)
-            logger.debug(f'found "{original}" in {event}, replaced with "{new}", result is {new_event}')
+            logger.debug(
+                f'found "{original}" in {event}, replaced with "{new}", result is {new_event}'
+            )
             event = new_event
     return event
 
@@ -100,13 +144,12 @@ def replace_event_names(event: str) -> str:
 def clear_rows(sheet: smartsheet.models.Sheet):
     if rows := [row.id for row in sheet.rows]:
         smart.Sheets.delete_rows(sheet_id=sheet.id, ids=rows)
-        logger.info(f'cleared {len(rows)} rows')
+        logger.info(f"cleared {len(rows)} rows")
     else:
-        logger.warning('no rows cleared')
+        logger.warning("no rows cleared")
 
 
-def write_rows(sheet: smartsheet.models.Sheet,
-               rows: list):
+def write_rows(sheet: smartsheet.models.Sheet, rows: list):
     new_rows = []
     col1, col2 = [col.id for col in sheet.columns[:2]]
     for name, date, color in rows:
@@ -128,14 +171,14 @@ def write_rows(sheet: smartsheet.models.Sheet,
             new_rows.append(new_row)
     if new_rows:
         smart.Sheets.add_rows(sheet.id, new_rows)
-        logger.info(f'wrote {len(new_rows)} rows')
+        logger.info(f"wrote {len(new_rows)} rows")
     else:
-        logger.warning('no rows written')
+        logger.warning("no rows written")
 
 
-def get_cell_by_column_name(row: smartsheet.models.Row,
-                            column_name: str,
-                            col_map: dict) -> smartsheet.models.Cell:
+def get_cell_by_column_name(
+    row: smartsheet.models.Row, column_name: str, col_map: dict
+) -> smartsheet.models.Cell:
     return row.get_column(col_map[column_name])  # {NAME: ID}
 
 
@@ -144,7 +187,7 @@ def column_name_to_id_map(columns: list) -> dict:
     return {column.title: column.id for column in columns}
 
 
-if __name__ == '__main__':
-    logger.info('starting program')
+if __name__ == "__main__":
+    logger.info("starting program")
     process_sheet(INTAKE_FORM_SHEET)
-    logger.info('program finished\n')
+    logger.info("program finished\n")
